9dbfc20cd6e31b2c73b338a2fbfe077b20ef9887
APKaramyshev <andrevvvvv@yandex.ru>
2021-02-22 11:40:01 +0300

    Initial Commit

---
+++ SwiftUI Library/SwiftUI_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LibraryApp.swift
+//  SwiftUI Library
+//
+//  Created by Andrey Karamyshev on 22.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
